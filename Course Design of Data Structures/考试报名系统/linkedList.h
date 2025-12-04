09a090c17daf18fb423b7c132abfb11fbc6cac07
cpurising <cpurising@outlook.com>
2025-12-04 18:43:57 +0800

    修改之前的遗漏问题

--- Course Design of Data Structures/考试报名系统/linkedList.h
+++ Course Design of Data Structures/考试报名系统/linkedList.h
@@ -1,24 +1,25 @@
 #pragma once
 #include<iostream>
 #include<iomanip>
+using namespace std;
 const int SPACE = 15;
 class LinkedNode
 {
 public:
-	std::string number;
-	std::string name;
+	string number;
+	string name;
 	bool sex;
 	int age;
-	std::string application;
+	string application;
 	LinkedNode* next;
 	LinkedNode();
-	LinkedNode(std::string number, std::string name, bool sex, int age, std::string application);
+	LinkedNode(string number, string name, bool sex, int age, std::string application);
 	LinkedNode(const LinkedNode& other);
 	LinkedNode& operator=(const LinkedNode& other);
 };
 
-bool safe_read_node(std::istream& is, LinkedNode& node);
-bool safe_read_num(std::istream& is, std::string& name);
+bool safe_read_node(istream& is, LinkedNode& node);
+bool safe_read_num(istream& is, string& name);
 
 class Linkedlist
 {
@@ -31,11 +32,11 @@ public:
 	int get_size();
 	void print_head_row();
 	void print_person(const LinkedNode& node);
-	LinkedNode* node_search(std::string num);//search "position"
-	void node_find(std::string num);//find and print
+	LinkedNode* node_search(string num);//search "position"
+	void node_find(string num);//find and print
 	void node_putback(const LinkedNode& node);
 	void node_insert(int index, const LinkedNode& node);//insert at "index",rather than"number"
-	void node_delete(std::string num);//delete at "number"
-	void node_edit(std::string num, const LinkedNode& node);//edit at "number"
+	void node_delete(string num);//delete at "number"
+	void node_edit(string num, const LinkedNode& node);//edit at "number"
 	void print_list();
 };
